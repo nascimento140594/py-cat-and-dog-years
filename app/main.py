92bf83f6635e93878a2843df1cfbe9e6581b70b3
nascimento140594 <nascimento140594@gmail.com>
2026-03-23 16:24:25 -0300

    Refactor human age calculation for cats and dogs

--- app/main.py
+++ app/main.py
@@ -1,15 +1,16 @@
 def get_human_age(cat_age: int, dog_age: int) -> list[int]:
-    cat_human_age = 0
-    dog_human_age = 0
-
-    if cat_age >= 15:
-        cat_human_age = 1
     if cat_age >= 24:
         cat_human_age = 2 + (cat_age - 24) // 4
+    elif cat_age >= 15:
+        cat_human_age = 1
+    else:
+        cat_human_age = 0
 
-    if dog_age >= 15:
-        dog_human_age = 1
     if dog_age >= 24:
         dog_human_age = 2 + (dog_age - 24) // 5
+    elif dog_age >= 15:
+        dog_human_age = 1
+    else:
+        dog_human_age = 0
 
     return [cat_human_age, dog_human_age]
